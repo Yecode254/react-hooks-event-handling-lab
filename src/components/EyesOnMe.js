a3dd349aae017e76ca6144342870e78644baee0e
Yego <eddieyego273@gmail.com>
2025-04-20 08:13:32 +0300

    completed assigment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+
+function EyesOnMe(){
+    function blur(){
+        return(console.log('Hey! Eyes on me!'))
+    }
+    function focus(){
+        return(console.log('Good!'))
+    }
+    return(
+        <div>
+<button onBlur={blur} onFocus={focus}>Eyes on me</button>
+</div>
+)
+};
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function passLoad(){
+        return( console.log('Entering password...'))
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={passLoad}></input>
+        </div>
     )
 }
 
